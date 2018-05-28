20b361f5d64b7d1928b99d279a0b2161144a4818
sujiahong <1511877962@qq.com>
2018-05-28 18:05:10 +0800

    modify file

--- game-server/coin-niu-server/handler.js
+++ game-server/coin-niu-server/handler.js
@@ -31,6 +31,66 @@ handle.login = function(socket, msg, next){
         logger.error("login_response: login failed. invalid sign");
         return next({code: 2});
     }
+    			//检查房间合法性
+	var userId = tokenMgr.getUserID(token);
+	//var roomId = roomMgr.getUserRoomId(userId);
+	socketSeqNo++;
+	if (socketSeqNo > 999999999) {
+		socketSeqNo = 1;
+	}
+	logger.info("login userId = %s", userId);
+	socket.seqno = socketSeqNo;
+	userMgr.bind(userId, socket);
+	//socket关联userid  roomid
+	socket.userId = userId;
+	socket.roomId = roomId;
+	//返回房间信息
+	var room = g_gameCoinRoom.getRoomById(roomId);
+	if (room == null) {
+		return next({code: 2});
+    }
+	room.setUserIP(userId, socket.handshake.address);
+	var userData = null;
+	var seats = [];
+	for (var i = 0, len = room.seats.length; i < len; ++i) {
+		var rs = room.seats[i];
+		var online = false;
+		if (rs.userId > 0) {
+			online = userMgr.isOnline(rs.userId);
+        }
+		seats.push({
+			userid: rs.userId,
+			ip: rs.ip,
+			score: rs.score,
+			name: rs.name,
+			online: online,
+			ready: rs.ready,
+			sex: rs.sex,
+			seatindex: i,
+			isowner: (parseInt(rs.userId) == parseInt(room.conf.creator))
+		})
+		if (userId == rs.userId) {
+			userData = seats[i];
+		}
+    }
+	//通知前端
+	var ret = {
+		code: errcode.OK,
+		roomid: room.id,
+		conf: room.conf,
+		numofgames: room.numOfGames,
+		seats: seats
+	};
+	logger.info('login_result');
+	logger.info(ret);
+	logger.info(seats);
+	
+	//通知其它客户端
+	userMgr.broacastInRoom('onJoinRoom', userData, userId)
+	socket.gameMgr = room.gameMgr
+	//玩家上线，强制设置为TRUE
+    socket.gameMgr.setReady(userId)
+    next(ret);
 }
 
 handle.robBanker = function(socket, msg, next){
